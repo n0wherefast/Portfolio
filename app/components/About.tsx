a398e74692d9bf5f2339b228dabeb9fff07cc36c
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2023-10-31 00:37:50 +0100

    cambiata misura immagina di about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,7 +5,7 @@ import coding from '../img/coding.jpg'
 function About() {
   return (
     <section className=' text-white' id="about">
-    <div className="container mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
+    <div className=" container mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
           Hi, `&apos;I`m Reed.
@@ -34,8 +34,8 @@ function About() {
         <Image 
         className="object-cover object-center rounded-2xl"
           alt="hero"
-          width={400}
-          height={400}
+          width={500}
+          height={500}
           src= {coding}/>        
       </div>
     </div>
