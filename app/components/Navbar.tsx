345bab30e4a94d3f570c4cff83dddc6e52848cb0
Alessio Russo <16mmstudios@gmail.com>
2023-11-01 21:51:29 +0100

    aggiunto componente Weather

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image'
 import { motion } from "framer-motion"
 import itaFlag from "../img/italy_flags_flag_17018.png"
 import engFlag from "../img/united_kingdom_flags_flag_17079.png"
+import Weather from './Weather'
 
 
 function Navbar() {
@@ -17,7 +18,7 @@ function Navbar() {
   const [isClick,setIsClick] = useState(false)
   const [width, setWidth] = useState(window.innerWidth)
 
-  const {isDarkMode,setIsDarkMode,handleDarkMode}:any = useGlobalContext()
+  const {isDarkMode,handleDarkMode}:any = useGlobalContext()
  
 
   const handleLangSwitch = () => {
@@ -46,10 +47,9 @@ function Navbar() {
   const  setMenu = () =>{
     return(
       <div  onClick={()=>setIsClick(!isClick)} className= {` cursor-pointer flex  md:inline-flex md:items-center md:border-0  text-xl mt-4 md:mt-0 `}>
-                          <button className="relative h-[30px] w-24 overflow-hidden rounded-xl px-3 transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-sky-500 before:transition-all ease-in before:duration-700 hover:text-white hover:before:w-full">
+                          <button className=" flex items-center justify-center relative h-[30px] w-24 overflow-hidden rounded-xl px-3 transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-sky-500 before:transition-all ease-in before:duration-700 hover:text-white hover:before:w-full">
                             <span className="flex relative z-10">Set
                             <IoIosArrowForward className={ isClick?`rotate-90  hover:text-white transition-all ease-in` : ` transition-all ease-in`} size={27} />
-
                             </span>
                           </button>
                         </div>
@@ -58,16 +58,19 @@ function Navbar() {
 
   
   return (
-    <header className = { isOpen?` transition-all duration-500 ease-in-out ${isDarkMode? 'bg-sky-950 text-white':'bg-slate-100'}   md:sticky top-0 z-10  ` : `transition-all duration-500 ease-in-out  ${isDarkMode?'md:bg-sky-950 text-white' : 'md:bg-slate-100 text-white md:text-black '} md:sticky top-0 z-10`}>
+    <header className = { isOpen?` transition-all duration-500 ease-in-out ${isDarkMode? 'bg-sky-950 text-white':'bg-slate-100'}   md:sticky top-0 z-10  ` : 
+                                  `transition-all duration-500 ease-in-out  ${isDarkMode?'md:bg-sky-950 text-white' : 'md:bg-slate-100 text-white md:text-black '} md:sticky top-0 z-10`}>
+
         <div className={isOpen ? ` h-96 transition-all ease-in-out duration-300  container mx-auto flex  p-3 flex-col  md:flex-row justify-between items-center md:justify-start` :
                                  ` h-24 transition-all ease-in-out duration-200  container mx-auto flex  p-3 flex-col  md:flex-row justify-between items-center md:justify-start`}>
                 
         <div className='flex flex-row w-full md:w-auto justify-between items-center m-2 md:p-0'>
                 <p className="title-font font-medium md:mb-0">
-                  <a href="#about" className={isOpen? `ml-3 text-3xl md:text-xl`:`ml-3 text-2xl`}>
+                  <a href="/" className={isOpen? `ml-3 text-3xl md:text-xl`:`ml-3 text-2xl`}>
                     Alessio Russo 
                   </a>
                 </p>
+                
                 <button className= {isOpen? `  flex rotate-180 md:hidden text-sky-500 transition-all ease-in duration-200`:` hover:bg-sky-900 p-2 rounded-full rotate-0 flex md:hidden text-white transition-all ease-in`  }
                         onClick={()=>(setIsOpen(!isOpen) ,setIsClick(false))}
                 >
@@ -76,6 +79,7 @@ function Navbar() {
         </div>
         <nav  className={isOpen? ` gap-2  w-full md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-600	flex  flex-col md:flex-row items-start text-base justify-start  ` :
                                  ` hidden  md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-600	md:flex  items-center text-base justify-center`}>
+             <hr /> <Weather/>    <hr />                
           <a href="#projects" className=" text-3xl md:text-xl  mr-5 py-1 px-3  rounded-lg hover:text-white hover:bg-sky-500 hover:animate-bounce">
              <span>Past Work</span>
           </a>
@@ -85,6 +89,7 @@ function Navbar() {
           {/* <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonials
           </a> */}
+          
         </nav>
          {isOpen? null: window.innerWidth > 800 ? setMenu() :null}
 
